604111d5cc7df4a6a78954487407fbacf39b3ab8
xinjie-just <18227752005@qq.com>
2020-12-06 18:21:28 +0800

    feat(*): 添加待办详情页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,15 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    noTodoList: false,
-    todoList: []
+    noTodoList: true,
+    todoList: [],
+    selectedId: null
   },
-  selectTodoItem(evt) {
-    console.log("evt111", evt);
+  selectTodoItem(e) {
+    console.log("evt111", e);
+    this.setData({
+      selectedId: e.currentTarget.dataset.id
+    });
+  },
+  handleItem() {
+    if(!this.data.selectedId) return;
+    console.log("selectedId",this.data.selectedId);
   },
   onLoad: function () {
     this.setData({
-      todoList: ['AA,阶段节点待你确认', 'BB,阶段节点待你确认','CC,阶段节点待你确认','DD,阶段节点待你确认']
+      todoList: [
+        { id: 1, title: 'AA,阶段节点待你确认' },
+        { id: 2, title: 'BB,阶段节点待你确认' },
+        { id: 3, title: 'CC,阶段节点待你确认' },
+        { id: 4, title: 'DD,阶段节点待你确认' },
+        { id: 5, title: 'EE,阶段节点待你确认' },
+        { id: 6, title: 'FF,阶段节点待你确认' },
+        { id: 7, title: 'GG,阶段节点待你确认' },
+        { id: 8, title: 'HH,阶段节点待你确认' },
+        { id: 9, title: 'II,阶段节点待你确认' }
+      ]
     })
   }
 })
